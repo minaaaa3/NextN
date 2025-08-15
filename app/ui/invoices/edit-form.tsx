f2b9a2c6645ad885f9b6ffdf1d2a0d2aa6a33adc
chiharu <qianqingjingshang312@gmail.com>
2025-08-15 20:51:14 +0900

    チュートリアル一応動く

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
 import {
   CheckIcon,
@@ -9,7 +8,8 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { updateInvoice } from "@/app/lib/actions";
+import { updateInvoice, State } from "@/app/lib/actions";
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +18,17 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(
+    async (prevState: State, formData?: FormData) => {
+      if (!formData) return prevState; // formData がない場合は prevState を返す
+      return await updateInvoice(invoice.id, formData);
+    },
+    initialState
+  );
 
-  return <form action={updateInvoiceWithId}>{/* ... */}</form>;
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
