7044778d8dbda4e3aeb966558cfd0ed0c9ea20f9
chiharu <qianqingjingshang312@gmail.com>
2025-08-18 19:00:34 +0900

    ビルドできた

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+export const dynamic = "force-dynamic";
+
 import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    clientReferences: true,
-  },
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  // Vercel 用に必要なら basePath や i18n を設定
 };
 
-module.exports = nextConfig;
+export default nextConfig;
